cfe94c9a3dd0a7530cc156d37538da2f92e7e725
Tutinichic <da295615@gmail.com>
2023-03-06 21:53:32 +0200

    added non-interacrive mode where you can input ABC from file

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,4 +1,6 @@
 const readline = require("readline");
+const path = require("path");
+const fs = require("fs");
 
 const readlineInterface = readline.createInterface({
   input: process.stdin,
@@ -6,10 +8,13 @@ const readlineInterface = readline.createInterface({
 });
 
 const INTERACTIVE = 2;
+const NON_INTERACTIVE = 3;
 
 (function () {
   if (process.argv.length === INTERACTIVE) {
     interactive();
+  } else if (process.argv.length === NON_INTERACTIVE) {
+    nonInteractive();
   }
 })();
 
@@ -22,25 +27,60 @@ async function interactive() {
 }
 
 function getABC(ourABC) {
-    return new Promise((resolve, reject) => {
-      readlineInterface.question(`${ourABC}:`, (yourNum) => {
-        const number = Number(yourNum);
-        let errorMsg;
-  
-        if (isNaN(number)) {
-          errorMsg = `Ми очікували від тебе число, a не це - ${yourNum} `;
-          console.log(errorMsg);
-          resolve(getABC(ourABC));
-        } else if (number === 0 && ourABC === "a") {
-          errorMsg = `Помилочка, "a" не може дорівнювати 0`;
-          console.log(errorMsg);
-          resolve(getABC(ourABC));
-        } else {
-          resolve(number);
-        }
-      });
+  return new Promise((resolve, reject) => {
+    readlineInterface.question(`${ourABC}:`, (yourNum) => {
+      const number = Number(yourNum);
+      let errorMsg;
+
+      if (isNaN(number)) {
+        errorMsg = `Ми очікували від тебе число, a не це - ${yourNum} `;
+        console.log(errorMsg);
+        resolve(getABC(ourABC));
+      } else if (number === 0 && ourABC === "a") {
+        errorMsg = `Помилочка, "a" не може дорівнювати 0`;
+        console.log(errorMsg);
+        resolve(getABC(ourABC));
+      } else {
+        resolve(number);
+      }
     });
-  }  
+  });
+}  
+
+function nonInteractive() {
+  const filePath = path.resolve(process.argv[2]);
+  try {
+    getABCFromFile(filePath);
+    readlineInterface.close();
+  } catch (error) {
+    console.log(`Поганий файл, ${filePath} не підходить`);
+    readlineInterface.close();
+  }
+}
+
+function getABCFromFile(file) {
+  const data = fs.readFileSync(file, "utf8");
+  const dataArray = data.split("\n");
+  const abcArray = dataArray[0].split(" ").map(parseFloat);
+  const [a, b, c] = abcArray;
+
+  if (abcArray.length != 3) {
+    console.log("Неправильні дані у файлі");
+    return;
+  } else if (a === 0) {
+    console.log(`Помилочка, "a" не може дорівнювати 0`);
+    return;
+  }
+
+  for (let ourABC of abcArray) {
+    let currentABC = Number(ourABC);
+    if (isNaN(currentABC)) {
+      console.log("Неправильні дані у файлі");
+      return;
+    }
+  }
+  calculate(a, b, c);
+}
 
 function calculate(a, b, c) {
   const labInfo = `\nТвоє рівняння: (${a}) x^2 + (${b}) x + (${c}) = 0\n`;

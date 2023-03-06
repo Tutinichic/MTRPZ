10b8e022c8f9d63cf273f01274f017286613ea95
Tutinichic <da295615@gmail.com>
2023-03-06 21:46:36 +0200

    added interactive mode where you can input ABC by terminal

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,3 +1,47 @@
+const readline = require("readline");
+
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const INTERACTIVE = 2;
+
+(function () {
+  if (process.argv.length === INTERACTIVE) {
+    interactive();
+  }
+})();
+
+async function interactive() {
+  const a = await getABC("a");
+  const b = await getABC("b");
+  const c = await getABC("c");
+  calculate(a, b, c);
+  readlineInterface.close();
+}
+
+function getABC(ourABC) {
+    return new Promise((resolve, reject) => {
+      readlineInterface.question(`${ourABC}:`, (yourNum) => {
+        const number = Number(yourNum);
+        let errorMsg;
+  
+        if (isNaN(number)) {
+          errorMsg = `Ми очікували від тебе число, a не це - ${yourNum} `;
+          console.log(errorMsg);
+          resolve(getABC(ourABC));
+        } else if (number === 0 && ourABC === "a") {
+          errorMsg = `Помилочка, "a" не може дорівнювати 0`;
+          console.log(errorMsg);
+          resolve(getABC(ourABC));
+        } else {
+          resolve(number);
+        }
+      });
+    });
+  }  
+
 function calculate(a, b, c) {
   const labInfo = `\nТвоє рівняння: (${a}) x^2 + (${b}) x + (${c}) = 0\n`;
   let solution, x1, x2;
